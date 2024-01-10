9d957547d12c29a19ce7ae42abc966f9da965fdb
Nurudeen Abdul-Majeed <abdulmajeednurudeen48@gmail.com>
2024-01-09 18:31:15 -0800

    shaping the responsiveness

--- src/js/responsive.js
+++ src/js/responsive.js
@@ -3,10 +3,13 @@ const hideDisplayNaBarElement = document.getElementById("logo-container");
 hideDisplayNaBarElement.addEventListener("click", () => {
   const navBarElement = document.getElementById("nav-bar");
   navBarElement.classList.toggle("nav-bar-hide");
+
   const allElementContainer = document.getElementById("container");
   allElementContainer.classList.toggle("container-only");
+
   const footerContainer = document.getElementById("footer-container");
   footerContainer.classList.toggle("without-nav-bar");
+
   const mainContentElement = document.getElementById("main-content");
   mainContentElement.classList.toggle("main-without-nav-bar");
 });
